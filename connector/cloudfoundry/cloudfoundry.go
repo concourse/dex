a69e630811b08f2449ada3f8bca1e271153e96a6
Kump3r <tonevkalin@gmail.com>
2025-01-07 12:28:51 +0200

    Refactor CloudFoundry API request handling to use updated response structure
    Signed-off-by: Kump3r <tonevkalin@gmail.com>

--- connector/cloudfoundry/cloudfoundry.go
+++ connector/cloudfoundry/cloudfoundry.go
@@ -94,7 +94,7 @@ func (c *Config) Open(id string, logger *slog.Logger) (connector.Connector, erro
 	}
 
 	apiURL := strings.TrimRight(c.APIURL, "/")
-	apiResp, err := cloudfoundryConn.httpClient.Get(fmt.Sprintf("%s/v3/info", apiURL))
+	apiResp, err := cloudfoundryConn.httpClient.Get(apiURL)
 	if err != nil {
 		return nil, fmt.Errorf("failed-to-send-request-to-cloud-controller-api: %w", err)
 	}
@@ -106,10 +106,17 @@ func (c *Config) Open(id string, logger *slog.Logger) (connector.Connector, erro
 		return nil, fmt.Errorf("failed-get-info-response-from-api: %w", err)
 	}
 
-	var apiResult map[string]interface{}
+	var apiResult struct {
+		Links struct {
+			Login struct {
+				Href string `json:"href"`
+			} `json:"login"`
+		} `json:"links"`
+	}
+
 	json.NewDecoder(apiResp.Body).Decode(&apiResult)
 
-	uaaURL := strings.TrimRight(apiResult["authorization_endpoint"].(string), "/")
+	uaaURL := strings.TrimRight(apiResult.Links.Login.Href, "/")
 	uaaResp, err := cloudfoundryConn.httpClient.Get(fmt.Sprintf("%s/.well-known/openid-configuration", uaaURL))
 	if err != nil {
 		return nil, fmt.Errorf("failed-to-send-request-to-uaa-api: %w", err)
